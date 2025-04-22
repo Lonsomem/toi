a3a699f436e3157f23f479b851ba1d72427abed5
lonsomem <hclee0730@gmail.com>
2025-04-22 12:44:08 +0900

    We've got a lot of things to do..

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def main():
 def chat(content_id, memory_id):
     memory['current chat'] = []
     memory['previous chat summary']=get_memory(memory_id)
+    load_memory(memory['previous chat summary'])
     while True:
         request = get_request_from_user() #for further develop, online!
         if request in ['exit', 'quit']:
             break
-        response = send_request(memory)
+        response = send_request(memory, request)
         show_response_to_user(response) #for further develop, online!
         memory['current chat'] = put_memory(memory['current chat'], request, response)
         if len(memory['current chat']) == 4:
@@ -26,13 +27,20 @@ def get_memory(memory_id):
     return
 
 def get_request_from_user(): 
-    return
+    request = input()
+    request_refined = [{"role":"user",
+                        "content":[{"type":"text", "text":request}]}]
+    return request_refined
 
-def send_request(memory):
-    return
+def send_request(memory, request):
+    full_request = memory + request
+    #send to API
+    return response
 
 def show_response_to_user(response): 
-    return
+    refined_response = refine_response(response)
+    print(refined_response)
+    return refined_response
 
 def summarize_chat():
     return
@@ -41,5 +49,6 @@ def save_memory():
     return
 
 def put_memory(current_chat_memory, request, response): #put in database, too
+    
     return
 
